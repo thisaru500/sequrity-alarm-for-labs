dccef308afedb37c1e7010f7482e5afe5f4ddfb5
thisaru500 <thisarudissanayake123@gmail.com>
2024-02-25 21:12:05 +0530

    Update sequrity_alarm_for_labs.ino
    add some sensors to my project (ldr  sensor and pir sensor)

--- sequrity_alarm_for_labs/sequrity_alarm_for_labs.ino
+++ sequrity_alarm_for_labs/sequrity_alarm_for_labs.ino
@@ -1,6 +1,8 @@
 #include<Wire.h>
 #include< LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3, POSITIVE);
+
 
 /*A0 = MQ 05 :- tocheaking ntral gases and DETECT LP GASES
  * NORMAL RANGE 230
@@ -11,7 +13,7 @@
  * VALUE = 60,180 NOMAL
  * VALUE>180 DANGER
  * 
- * A2 = LDR SENSOR
+ * A2 = LDR SENSOR  sensor value>200 it is day time SENSOR value<100 it's night time
  * A3 = TEMPURATURE SENSOR LM-35  TEMPURATURE = VOLTAGE*100
  */
 
@@ -19,13 +21,61 @@ int pirsensor = 3;
 int flamesensor = 4;
 int buzzer1 = 5;//gas sensor buzzer
 int buzzer2 = 6;//air quality buzzer , fire buzzer
-int relay = 7;//for turn on 230v led light
+int relay1 = 7;//for turn on 230v led light
+int relay2 = 10;// turn on door motor
+int rled = 8;// red led for day time
+int gled = 9;// green led for night time
+
+
 void setup() {
+ Serial.begin(9600);
+ lcd.begin(16,4);
  
+pinMode(pirsensor,INPUT);
+pinMode(flamesensor,INPUT);
+pinMode(buzzer1,OUTPUT);
+pinMode(buzzer2,OUTPUT);
+pinMode(relay1,OUTPUT);
+pinMode(relay2,OUTPUT);
 
 }
 
 void loop() {
+
+  // Geting analog values
+
+  int lpgas = analogRead(A0); //to get gas sensor values
+  int airquality = analogRead(A1); // to get air quality walues
+  int ldr = analogRead(A2); //to get ldr values
+  int tempsen = analogRead(A3); // to get tempurature values
+
+  //get digital values
+  int pirval = digitalRead(pirsensor);// to get pir sensor reading
+  int flameval = digitalRead(flamesensor);// to get flame sensor reading
+
+  if(ldr>100){
+    digitalWrite(relay1,HIGH);//to turn on 230v led light
+    digitalWrite(gled,HIGH); // TO TURN ON GREEN LED
+    }
+
+    else{
+      digitalWrite(relay1,LOW);
+      digitalWrite(rled,HIGH);// TO  turn on red led
+      }
+
+      if(pirval == 1){
+        digitalWrite(relay2,HIGH);
+        delay (1000);
+        digitalWrite(relay2,LOW);
+        }
+
+        else{
+          digitalWrite(relay2,LOW);
+        }
+
+        
+      
+  
   
 
 }
