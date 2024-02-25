5f0d1d1a8510aeefcfa62c98f97cfc9bac6feb6a
thisaru500 <thisarudissanayake123@gmail.com>
2024-02-25 11:28:36 +0530

    Update sequrity_alarm_for_labs.ino
    creating pins and find sensors values for my project and write comments on my arduino IDE

--- sequrity_alarm_for_labs/sequrity_alarm_for_labs.ino
+++ sequrity_alarm_for_labs/sequrity_alarm_for_labs.ino
@@ -1,5 +1,27 @@
+#include<Wire.h>
+#include< LiquidCrystal_I2C.h>
+
+
+/*A0 = MQ 05 :- tocheaking ntral gases and DETECT LP GASES
+ * NORMAL RANGE 230
+ * 230-550 NORML GAS POWER BUT DANGER
+ * GAS POWER>550 DANGER AND TURN ON BUZZER
+ * 
+ *A1= MQ-135 :- TO CHEAKING AIR QUALITY
+ * VALUE = 60,180 NOMAL
+ * VALUE>180 DANGER
+ * 
+ * A2 = LDR SENSOR
+ * A3 = TEMPURATURE SENSOR LM-35  TEMPURATURE = VOLTAGE*100
+ */
+
+int pirsensor = 3;
+int flamesensor = 4;
+int buzzer1 = 5;//gas sensor buzzer
+int buzzer2 = 6;//air quality buzzer , fire buzzer
+int relay = 7;//for turn on 230v led light
 void setup() {
-  
+ 
 
 }
 
